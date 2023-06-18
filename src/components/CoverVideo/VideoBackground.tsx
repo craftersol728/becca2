7393d89246d427100999f36bc098faad0ba9a324
Jerry Wan <craftersol728@gmail.com>
2023-06-18 13:50:08 -0700

    added a dark hue to the video

--- src/components/CoverVideo/VideoBackground.tsx
+++ src/components/CoverVideo/VideoBackground.tsx
@@ -5,9 +5,15 @@ const VideoBackground: React.FC = () => {
   return (
     <div className="video-background">
       <video autoPlay muted loop className="video">
-        <source src="public\coverVideo.mp4" type="video/mp4" />
+        <source src="\coverVideo.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+
+      <div className="content">
+        <h1 className="title">Your Title</h1>
+        <p className="subtitle">Your Subtitle</p>
+      </div>
+
     </div>
   )
 }
