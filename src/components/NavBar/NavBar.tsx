c48941f1554fded2215e66297b0640d6ca2ddb1b
Jerry Wan <craftersol728@gmail.com>
2023-06-18 14:29:18 -0700

    added NavBar links

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <div className="nav-bar">
+      <NavLink to="/about-us" className={({ isActive }) => isActive ? 'active' : ''}>About Us</NavLink>
+      <NavLink to="/services" className={({ isActive }) => isActive ? 'active' : ''}>Services</NavLink>
+      <NavLink to="/gallery" className={({ isActive }) => isActive ? 'active' : ''}>Gallery/Testimonials</NavLink>
+      <NavLink to="/faq" className={({ isActive }) => isActive ? 'active' : ''}>FAQ</NavLink>
+      <NavLink to="/contact" className={({ isActive }) => isActive ? 'active' : ''}>Contact/Booking</NavLink>
+    </div>
+  )
+}
+
+export default NavBar;
