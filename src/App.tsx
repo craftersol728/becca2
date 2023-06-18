7edbb8bc7cf81ccbd16e753864af871718471317
Jerry Wan <craftersol728@gmail.com>
2023-06-18 16:47:55 -0700

    initialized Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import './App.css';
 
 //import { useState } from 'react';
 import VideoBackground from '../src/components/CoverVideo/VideoBackground';  // Imported the VideoBackground component
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
 
   return (
     <>
       <VideoBackground /> 
-      
+       {/* <NavBar /> */}
   
     </>
   );
