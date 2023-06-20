14cec4b9abd7d620176894bf39373fae3d5d29eb
Jerry Wan <craftersol728@gmail.com>
2023-06-19 21:03:36 -0700

    started nav-bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './dist/main.css'
 
 import { Hero } from './components/Hero/Hero'
+import NavBar from './components/NavBar/NavBar'
+
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
 
 function App() {
   return (
     <>
-      <Hero heroType="video" source="/coverVideo.mp4" title="Title" subTitle="Sub Title" />
+      <Router>
+        <NavBar />
+        <Hero heroType="video" source="/coverVideo.mp4" title="Title" subTitle="Sub Title" />
+      </Router>
     </>
   )
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="nav-bar">
+      <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''}>Icon</NavLink>
       <NavLink to="/about-us" className={({ isActive }) => isActive ? 'active' : ''}>About Us</NavLink>
       <NavLink to="/services" className={({ isActive }) => isActive ? 'active' : ''}>Services</NavLink>
       <NavLink to="/gallery" className={({ isActive }) => isActive ? 'active' : ''}>Gallery/Testimonials</NavLink>
