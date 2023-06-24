de9634d9a1c7bb38d182c8f809d31ac307180956
Jerry Wan <craftersol728@gmail.com>
2023-06-23 20:15:35 -0700

    added hamburger

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,17 +1,63 @@
 // import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { ReactComponent as Hamburger } from '../../../public/assets/Hamburger_icon.svg.png'
+import { ReactComponent as Brand } from '../../../public/assets/Logo.png'
 
 const NavBar = () => {
+  const [showNavbar, setShowNavbar] = useState(false)
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+
   return (
     <div className="nav-bar">
-      <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''}>Icon</NavLink>
-      <NavLink to="/about-us" className={({ isActive }) => isActive ? 'active' : ''}>About Us</NavLink>
-      <NavLink to="/services" className={({ isActive }) => isActive ? 'active' : ''}>Services</NavLink>
-      <NavLink to="/gallery" className={({ isActive }) => isActive ? 'active' : ''}>Gallery/Testimonials</NavLink>
-      <NavLink to="/faq" className={({ isActive }) => isActive ? 'active' : ''}>FAQ</NavLink>
-      <NavLink to="/contact" className={({ isActive }) => isActive ? 'active' : ''}>Contact/Booking</NavLink>
+      <div className="container">
+        <div className="logo">
+          <Brand />
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <Hamburger />
+        </div>
+      </div>
+
+      <div className={`nav-elements  ${showNavbar && 'active'}`}>
+        <ul>
+          <li>
+            <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
+              Icon
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about-us" className={({ isActive }) => (isActive ? 'active' : '')}>
+              About Us
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/services" className={({ isActive }) => (isActive ? 'active' : '')}>
+              Services
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/gallery" className={({ isActive }) => (isActive ? 'active' : '')}>
+              Gallery/Testimonials
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/faq" className={({ isActive }) => (isActive ? 'active' : '')}>
+              FAQ
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" className={({ isActive }) => (isActive ? 'active' : '')}>
+              Contact/Booking
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
 
-export default NavBar;
+export default NavBar
