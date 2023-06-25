56a28a6fc6a119528ce4d0548fd9eec770d62e7d
Chenyi <zou.chenyi@yahoo.com>
2023-06-24 23:12:43 -0500

    scrollbar update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import './dist/main.css'
+import 'locomotive-scroll/dist/locomotive-scroll.css'
 
 import { Hero } from './components/Hero/Hero'
 import NavBar from './components/NavBar/NavBar'
 
-import { BrowserRouter as Router } from 'react-router-dom';
-import Summary from './components/Summary/Summary';
-
-
+import { BrowserRouter as Router } from 'react-router-dom'
+import Summary from './components/Summary/Summary'
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
+import { useRef } from 'react'
 
 function App() {
+  const containerRef = useRef<any>(null)
+
   return (
     <>
       <Router>
-        <NavBar />
-        <Hero heroType="video" source="/coverVideo.mp4" title="Title" subTitle="Sub Title" />
-        <Summary />
-        <Summary />
-        <Summary />
-        <Summary />
-        <Summary />
-        <Summary />
-        
+        <LocomotiveScrollProvider
+          options={{
+            smooth: true,
+            // ... all available Locomotive Scroll instance options
+          }}
+          watch={
+            [
+              //..all the dependencies you want to watch to update the scroll.
+              //  Basicaly, you would want to watch page/location changes
+              //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+            ]
+          }
+          containerRef={containerRef}
+        >
+          <main data-scroll-container ref={containerRef}>
+            <NavBar />
+            <Hero heroType="video" source="/coverVideo.mp4" title="Title" subTitle="Sub Title" />
+            <Summary />
+            <Summary />
+            <Summary />
+            <Summary />
+            <Summary />
+            <Summary />
+          </main>
+        </LocomotiveScrollProvider>
       </Router>
     </>
   )
